76a89a0d2be5e51995e5845736eba8eab95d2e16
bizahmad <ahmad_baiazid@hotmail.com>
2020-02-16 23:06:10 -0500

    finished lec2

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/bizahmad/go-Microservices/handlers"
 )
@@ -17,5 +20,35 @@ func main() {
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
-	http.ListenAndServe(":8080", sm)
+
+	//creating our own server config
+	//More control over the server's behavior is available by creating a custom Server:
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	//start the server
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt) //whenever a signal is received by the system itll send the message on that channel
+	signal.Notify(sigChan, os.Kill)      //trap a forceful kill to the program
+
+	// Block until a signal is received.
+	sig := <-sigChan
+	l.Println("Received terminate, gracefgul shutdown", sig)
+
+	// if the handlers are working after 30 seconds then shut them down forcefully
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
